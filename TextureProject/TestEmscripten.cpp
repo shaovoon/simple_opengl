45af05e7f7e2680e574fe27662834251714fadd6
shaovoon <shaovoon@yahoo.com>
2019-08-23 18:22:11 +0800

    Add texture project. Add compiled html and js

--- TextureProject/TestEmscripten.cpp
+++ TextureProject/TestEmscripten.cpp
@@ -5,11 +5,10 @@ and may not be redistributed without written permission.*/
 #include <SDL.h>
 #undef main 
 
-#ifdef __EMSCRIPTEN__
-	#include <emscripten.h>
-#endif
-
 #include "GL/glew.h"
+//#include <GL/GLU.h>
+//#include <SDL_opengl.h>
+#include <SDL_image.h> //Needed for IMG_Load.  If you want to use bitmaps (SDL_LoadBMP), it appears to not be necessary
 #include <stdio.h>
 #include <string>
 
@@ -17,17 +16,17 @@ and may not be redistributed without written permission.*/
 #define GLM_FORCE_PURE
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
-#include "glm/gtx/rotate_vector.hpp"
-#include "glm/gtx/transform.hpp"
 
 #ifdef __EMSCRIPTEN__
+#include <emscripten.h>
 #include <emscripten/html5.h>
-//#include <GLES2/gl2.h>
 #endif
+
+#define IMG_FILE "yes.png"
 
 struct UserData
 {
-	UserData() : programObject(0), wmpLoc(-1), positionLoc(-1), colorLoc(-1) {}
+	UserData() : programObject(0), wmpLoc(-1), positionLoc(-1), texCoordLoc(-1), samplerLoc(-1), textureId(0), images_loaded(0) {}
 	// Handle to a program object
 	GLuint programObject;
 
@@ -35,7 +34,15 @@ struct UserData
 
 	// Attribute locations
 	GLint  positionLoc;
-	GLint  colorLoc;
+	GLint  texCoordLoc;
+
+	// Sampler location
+	GLint samplerLoc;
+
+	// Texture handle
+	GLuint textureId;
+	int images_loaded;
+
 };
 
 //Screen dimension constants
@@ -53,17 +60,23 @@ GLuint LoadShader(GLenum type, const char *shaderSrc);
 //Input handler
 void handleKeys(unsigned char key, int x, int y);
 
+//Per frame update
+void update();
+
+GLuint init_texture(const char * file);
+void load_texture(const char * file);
+void load_error(const char * file);
+
+
+void deinit_texture(GLuint texture);
+
 //Renders quad to the screen
 void render();
-void updatePosition(glm::mat4x4& mat);
-void updateColor();
+void updatePosition();
 
 void createVBO(GLfloat *vertexBuffer,
 	GLushort *indices, GLuint numVertices, GLuint numIndices,
 	GLuint& vboId, GLuint& indexId, GLuint numElems, GLenum usage);
-
-void createVBOArray(GLfloat *vertexBuffer, GLuint numVertices,
-	GLuint& vboId, GLuint numElems, GLenum usage);
 
 //Frees media and shuts down SDL
 void close();
@@ -82,8 +95,6 @@ UserData gUserData;
 
 GLuint gVboId;
 GLuint gIndexId;
-GLuint gVboColorId;
-GLuint gIndexColorId;
 
 const float DefaultNearPlaneDistance = 0.01f;
 const float DefaultFarPlaneDistance = 1000.0f;
@@ -91,9 +102,6 @@ const float DefaultFarPlaneDistance = 1000.0f;
 glm::mat4 gViewMatrix;
 glm::mat4 gProjectionMatrix;
 glm::mat4 gWorldMatrix;
-
-#define VERT_POS_INDEX 0
-#define COLOR_INDEX 1
 
 int main()
 {
@@ -110,8 +118,6 @@ int main()
 		//Event handler
 		SDL_Event e;
 
-		//Enable text input
-		SDL_StartTextInput();
 #ifdef __EMSCRIPTEN__
 		emscripten_set_main_loop(render, 0, 0);
 #else
@@ -135,15 +141,17 @@ int main()
 				}
 			}
 
-			render();
-
+			if (gUserData.images_loaded >= 1)
+			{
+				//Render quad
+				render();
+			}
 			//Update screen
 			SDL_GL_SwapWindow(gWindow);
 		}
 #endif
-		//Disable text input
-		SDL_StopTextInput();
 
+		deinit_texture(gUserData.textureId);
 	}
 
 	//Free resources and close SDL
@@ -244,24 +252,30 @@ bool init(UserData *userData)
 
 bool initGL(UserData *userData)
 {
+#ifdef __EMSCRIPTEN__
+	emscripten_async_wget("http://localhost:16564/quad2.png", IMG_FILE, load_texture, load_error);
+#endif
+
 	const char vShaderStr [] =
 		"precision mediump float;     \n"
 		"uniform mat4 WorldViewProjection;\n"
 		"attribute vec3 a_position;   \n"
-		"attribute vec4 a_color;   \n"
-		"varying vec4 v_color;   \n"
+		"attribute vec2 a_texCoord;   \n"
+		"varying vec2 v_texCoord;     \n"
 		"void main()                  \n"
-		"{  v_color = a_color;                          \n"
+		"{                            \n"
 		"   gl_Position = WorldViewProjection * vec4(a_position, 1.0); \n"
+		"   v_texCoord = a_texCoord;  \n"
 		"}                            \n";
 
 	const char fShaderStr [] =
 		"precision mediump float;     \n"
-		"varying vec4 v_color;   \n"
-		"void main()                                  \n"
-		"{                                            \n"
-		"  gl_FragColor = v_color;      \n"
-		"}                                            \n";
+		"varying vec2 v_texCoord;                            \n"
+		"uniform sampler2D s_texture;                        \n"
+		"void main()                                         \n"
+		"{                                                   \n"
+		"  gl_FragColor = texture2D( s_texture, v_texCoord );\n"
+		"}                                                   \n";
 
 	GLuint vertexShader;
 	GLuint fragmentShader;
@@ -317,35 +331,32 @@ bool initGL(UserData *userData)
 	userData->wmpLoc = glGetUniformLocation(userData->programObject, "WorldViewProjection");
 
 	// Get the attribute locations
-	//userData->positionLoc = glGetAttribLocation(userData->programObject, "a_position");
+	userData->positionLoc = glGetAttribLocation(userData->programObject, "a_position");
+	userData->texCoordLoc = glGetAttribLocation(userData->programObject, "a_texCoord");
 
-	// Get the attribute locations
-	//userData->colorLoc = glGetAttribLocation(userData->programObject, "a_color");
+	// Get the sampler location
+	userData->samplerLoc = glGetUniformLocation(userData->programObject, "s_texture");
 
-	GLfloat vVertices [] = { -1.0f, 1.0f, 0.0f,  // Position 0
-		-1.0f, -1.0f, 0.0f,  // Position 1
-		1.0f, -1.0f, 0.0f,  // Position 2
-		1.0f, 1.0f, 0.0f,  // Position 3
+	// Load the texture
+#ifndef __EMSCRIPTEN__
+	userData->textureId = init_texture(IMG_FILE);
+	++gUserData.images_loaded;
+#endif
+
+	GLfloat vVertices [] = { -0.5f, 0.5f, 0.0f,  // Position 0
+		0.0f, 0.0f,        // TexCoord 0 
+		-0.5f, -0.5f, 0.0f,  // Position 1
+		0.0f, 1.0f,        // TexCoord 1
+		0.5f, -0.5f, 0.0f,  // Position 2
+		1.0f, 1.0f,        // TexCoord 2
+		0.5f, 0.5f, 0.0f,  // Position 3
+		1.0f, 0.0f         // TexCoord 3
 	};
 	GLushort indices [] = { 0, 1, 2, 0, 2, 3 };
 
-	GLfloat vColors [] = { 1.0, 1.0, 0.0, 0.0, 
-		1.0, 1.0, 0.0, 0.0, 
-		1.0, 1.0, 0.0, 0.0, 
-		1.0, 1.0, 0.0, 0.0 };
-
 	createVBO(&vVertices[0],
 		&indices[0], 4, sizeof(indices) / sizeof(GLushort),
-		gVboId, gIndexId, 3, GL_DYNAMIC_DRAW);
-
-	glEnableVertexAttribArray(VERT_POS_INDEX);
-	glBindAttribLocation(gUserData.programObject, VERT_POS_INDEX, "a_position");
-
-	createVBOArray(&vColors[0],
-		4, gVboColorId, 4, GL_DYNAMIC_DRAW);
-
-	glEnableVertexAttribArray(COLOR_INDEX);
-	glBindAttribLocation(gUserData.programObject, COLOR_INDEX, "a_color");
+		gVboId, gIndexId, 5, GL_DYNAMIC_DRAW);
 
 	// Set the viewport
 	glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
@@ -372,16 +383,6 @@ void createVBO(GLfloat *vertexBuffer,
 		usage);
 }
 
-void createVBOArray(GLfloat *vertexBuffer, GLuint numVertices, 
-	GLuint& vboId, GLuint numElems, GLenum usage)
-{
-	// vertex
-	glGenBuffers(1, &vboId);
-	glBindBuffer(GL_ARRAY_BUFFER, vboId);
-	glBufferData(GL_ARRAY_BUFFER, numVertices*(sizeof(GLfloat) * numElems),
-		vertexBuffer, usage);
-}
-
 void handleKeys(unsigned char key, int x, int y)
 {
 	//Toggle quad
@@ -391,67 +392,85 @@ void handleKeys(unsigned char key, int x, int y)
 	}
 }
 
+void update()
+{
+	//No per frame update needed
+}
+
 void render()
 {
+	glBindBuffer(GL_ARRAY_BUFFER, gVboId);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, gIndexId);
+
 	// Clear the color buffer
 	glClear(GL_COLOR_BUFFER_BIT);
 
 	// Use the program object
 	glUseProgram(gUserData.programObject);
 
-	glEnableVertexAttribArray(VERT_POS_INDEX);
-	glBindBuffer(GL_ARRAY_BUFFER, gVboId);
-
-	glVertexAttribPointer(VERT_POS_INDEX, 3, GL_FLOAT,
-		GL_FALSE, 3 * sizeof(GLfloat), (const void*) 0);
-
-	glEnableVertexAttribArray(COLOR_INDEX);
-	glBindBuffer(GL_ARRAY_BUFFER, gVboColorId);
-
-	glVertexAttribPointer(COLOR_INDEX, 4, GL_FLOAT,
-		GL_FALSE, 4*sizeof(GLfloat), (const void*) 0);
-
-	updateColor();
-
-	glm::vec3 eye(0, 0, 2);
 	glm::vec3 direction = glm::vec3(0.0f, 0.0f, -1.0f);
 	glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
 
-	glm::vec3 target = eye + direction;
-	gViewMatrix = glm::lookAt(eye, target, up);
+	glm::vec3 position(0, 0, 2);
 
-	//gProjectionMatrix = glm::ortho(-1.0f, 1.0f, -0.75f, 0.75f, DefaultNearPlaneDistance, DefaultFarPlaneDistance);
-	gProjectionMatrix = glm::perspective(30.0f, 1.0f / 0.75f, 1.0f, 20.0f);
+	glm::vec3 target = position + direction;
+	gViewMatrix = glm::lookAt(position, target, up);
 
-	glm::mat4x4 mat;
-	updatePosition(mat);
-	glm::mat4 wvp = gProjectionMatrix * gViewMatrix * mat;
+	gProjectionMatrix = glm::ortho(-1.0f, 1.0f, -0.75f, 0.75f, DefaultNearPlaneDistance, DefaultFarPlaneDistance);
+
+	glm::mat4 wvp = gProjectionMatrix * gViewMatrix * gWorldMatrix;
 	glUniformMatrix4fv(gUserData.wmpLoc, 1, GL_FALSE, &wvp[0][0]);
+
+	updatePosition();
+
+	// Load the vertex position
+	glVertexAttribPointer(gUserData.positionLoc, 3, GL_FLOAT,
+		GL_FALSE, 5 * sizeof(GLfloat), (const void*) 0);
+	// Load the texture coordinate
+	glVertexAttribPointer(gUserData.texCoordLoc, 2, GL_FLOAT,
+		GL_FALSE, 5 * sizeof(GLfloat), (const void*) 12);
+
+	glEnableVertexAttribArray(gUserData.positionLoc);
+	glEnableVertexAttribArray(gUserData.texCoordLoc);
+
+	// Bind the texture
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, gUserData.textureId);
+
+	// Set the sampler texture unit to 0
+	glUniform1i(gUserData.samplerLoc, 0);
 
 	// Draw 
 	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, 0);
 }
 
-void updatePosition(glm::mat4x4& mat)
+void updatePosition()
 {
 	double total_time = SDL_GetTicks() / 1000.0;
+	GLfloat mod2 = fmod(total_time, 4.0);
+	GLfloat mod4 = fmod(total_time, 8.0);
+	bool to_right = mod4 >= 4.0;
+	GLfloat delta = 0.0;
+	if (to_right)
+		delta = 0.1*(mod2 - 2.0);
+	else
+		delta = 0.1*((4.0 - mod2) - 2.0);
 
-	GLfloat half_PI = static_cast<GLfloat>(M_PI) / 2.0f;
-	GLfloat total_animated_time = 4.0f;
-	GLfloat mod4 = fmod(total_time, total_animated_time);
+	GLfloat min_size = 0.4;
+	GLfloat scale = 0.5f * delta + min_size;
+	GLfloat left = -1.0*scale;
+	GLfloat top = 1.0*scale;
+	GLfloat right = 1.0*scale;
+	GLfloat bottom = -1.0*scale;
 
-	GLfloat y_angle = half_PI * mod4;
-	mat = glm::rotate(y_angle, glm::vec3(0.0f, 1.0f, 0.0f));
-}
+	GLfloat vVertices [] = {
+		left, top, 0.0f, 0.0f, 0.0f,
+		left, bottom, 0.0f, 0.0f, 1.0f,
+		right, bottom, 0.0f, 1.0f, 1.0f,
+		right, top, 0.0f, 1.0f, 0.0f
+	};
 
-void updateColor()
-{
-	GLfloat vColors [] = { 1.0, 0.0, 0.0, 0.0, 
-			1.0, 0.0, 0.0, 0.0, 
-			1.0, 0.0, 0.0, 0.0, 
-			1.0, 0.0, 0.0, 0.0 };
-	
-	glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vColors), (const GLvoid *) vColors);
+	glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vVertices), (const GLvoid *) vVertices);
 }
 
 void close()
@@ -508,4 +527,63 @@ GLuint LoadShader(GLenum type, const char *shaderSrc)
 	return shader;
 }
 
+void load_texture(const char * file)
+{
+	gUserData.textureId = init_texture(file);
+	++gUserData.images_loaded;
+}
+
+void load_error(const char * file)
+{
+	printf("File download failed: %s", file);
+}
+
+GLuint init_texture(const char * file) {
+	GLuint texture = 0;
+	//Load the image from the file into SDL's surface representation
+	SDL_Surface* surface = IMG_Load(file);
+	if (surface == NULL) { //If it failed, say why and don't continue loading the texture
+		printf("Error: \"%s\"\n", SDL_GetError()); return 0;
+	}
+
+	//Generate an array of textures.  We only want one texture (one element array), so trick
+	//it by treating "texture" as array of length one.
+	glGenTextures(1, &texture);
+	//Select (bind) the texture we just generated as the current 2D texture OpenGL is using/modifying.
+	//All subsequent changes to OpenGL's texturing state for 2D textures will affect this texture.
+	glBindTexture(GL_TEXTURE_2D, texture);
+	//Specify the texture's data.  This function is a bit tricky, and it's hard to find helpful documentation.  A summary:
+	//   GL_TEXTURE_2D:    The currently bound 2D texture (i.e. the one we just made)
+	//               0:    The mipmap level.  0, since we want to update the base level mipmap image (i.e., the image itself,
+	//                         not cached smaller copies)
+	//         GL_RGBA:    The internal format of the texture.  This is how OpenGL will store the texture internally (kinda)--
+	//                         it's essentially the texture's type.
+	//      surface->w:    The width of the texture
+	//      surface->h:    The height of the texture
+	//               0:    The border.  Don't worry about this if you're just starting.
+	//          GL_RGB:    The format that the *data* is in--NOT the texture!  Our test image doesn't have an alpha channel,
+	//                         so this must be RGB.
+	//GL_UNSIGNED_BYTE:    The type the data is in.  In SDL, the data is stored as an array of bytes, with each channel
+	//                         getting one byte.  This is fairly typical--it means that the image can store, for each channel,
+	//                         any value that fits in one byte (so 0 through 255).  These values are to be interpreted as
+	//                         *unsigned* values (since 0x00 should be dark and 0xFF should be bright).
+	// surface->pixels:    The actual data.  As above, SDL's array of bytes.
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, surface->w, surface->h, 0, GL_RGBA, GL_UNSIGNED_BYTE, surface->pixels);
+	//Set the minification and magnification filters.  In this case, when the texture is minified (i.e., the texture's pixels (texels) are
+	//*smaller* than the screen pixels you're seeing them on, linearly filter them (i.e. blend them together).  This blends four texels for
+	//each sample--which is not very much.  Mipmapping can give better results.  Find a texturing tutorial that discusses these issues
+	//further.  Conversely, when the texture is magnified (i.e., the texture's texels are *larger* than the screen pixels you're seeing
+	//them on), linearly filter them.  Qualitatively, this causes "blown up" (overmagnified) textures to look blurry instead of blocky.
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	//Unload SDL's copy of the data; we don't need it anymore because OpenGL now stores it in the texture.
+	SDL_FreeSurface(surface);
+
+	return texture;
+}
+void deinit_texture(GLuint texture) {
+	//Deallocate an array of textures.  A lot of people forget to do this.
+	glDeleteTextures(1, &texture);
+}
 
