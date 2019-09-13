d0b5174ec146412623d951d4b165b2c24802a41e
shaovoon <shaovoon@yahoo.com>
2019-09-13 19:23:29 +0800

    Can load jpg image as texture

--- LocalTextureProject/LocalTextureProject.cpp
+++ LocalTextureProject/LocalTextureProject.cpp
@@ -12,6 +12,9 @@ and may not be redistributed without written permission.*/
 #include <stdio.h>
 #include <string>
 #include <vector>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
 #include "base64.h"
 
 #define GLM_FORCE_RADIANS
@@ -25,7 +28,7 @@ and may not be redistributed without written permission.*/
 #endif
 
 #define IMG_FILE "yes.png"
-#define IMG_FILE2 "quad2.png"
+#define IMG_FILE2 "joeyswee.jpg"
 
 struct UserData
 {
@@ -346,8 +349,8 @@ bool initGL(UserData *userData)
 	userData->textureId2 = init_texture_from_memory_wrapper(IMG_FILE);
 	++gUserData.images_loaded;
 	//userData->textureId2 = init_texture(IMG_FILE2);
-	//userData->textureId2 = init_texture_from_memory_wrapper(IMG_FILE2);
-	//++gUserData.images_loaded;
+	userData->textureId2 = init_texture_from_memory_wrapper(IMG_FILE2);
+	++gUserData.images_loaded;
 #endif
 
 	GLfloat vVertices [] = { -0.5f, 0.5f, 0.0f,  // Position 0
@@ -630,6 +633,53 @@ GLuint init_texture_from_memory(unsigned char* bytes, int size) {
 	return texture;
 }
 
+GLuint init_jpeg_texture_from_memory(unsigned char* bytes, int size) {
+	GLuint texture = 0;
+	int width, height, channels;
+	//stbi_set_flip_vertically_on_load(true);
+	unsigned char* image = stbi_load_from_memory(bytes, size,
+		&width,
+		&height,
+		&channels,
+		STBI_rgb);
+	if (image == NULL) { //If it failed, say why and don't continue loading the texture
+		printf("Error: \"%s\"\n", SDL_GetError()); return 0;
+	}
+
+	//Generate an array of textures.  We only want one texture (one element array), so trick
+	//it by treating "texture" as array of length one.
+	glGenTextures(1, &texture);
+	//Select (bind) the texture we just generated as the current 2D texture OpenGL is using/modifying.
+	//All subsequent changes to OpenGL's texturing state for 2D textures will affect this texture.
+	glBindTexture(GL_TEXTURE_2D, texture);
+	//Specify the texture's data.  This function is a bit tricky, and it's hard to find helpful documentation.  A summary:
+	//   GL_TEXTURE_2D:    The currently bound 2D texture (i.e. the one we just made)
+	//               0:    The mipmap level.  0, since we want to update the base level mipmap image (i.e., the image itself,
+	//                         not cached smaller copies)
+	//         GL_RGBA:    The internal format of the texture.  This is how OpenGL will store the texture internally (kinda)--
+	//                         it's essentially the texture's type.
+	//      surface->w:    The width of the texture
+	//      surface->h:    The height of the texture
+	//               0:    The border.  Don't worry about this if you're just starting.
+	//          GL_RGB:    The format that the *data* is in--NOT the texture!  Our test image doesn't have an alpha channel,
+	//                         so this must be RGB.
+	//GL_UNSIGNED_BYTE:    The type the data is in.  In SDL, the data is stored as an array of bytes, with each channel
+	//                         getting one byte.  This is fairly typical--it means that the image can store, for each channel,
+	//                         any value that fits in one byte (so 0 through 255).  These values are to be interpreted as
+	//                         *unsigned* values (since 0x00 should be dark and 0xFF should be bright).
+	// surface->pixels:    The actual data.  As above, SDL's array of bytes.
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+	//Set the minification and magnification filters.  In this case, when the texture is minified (i.e., the texture's pixels (texels) are
+	//*smaller* than the screen pixels you're seeing them on, linearly filter them (i.e. blend them together).  This blends four texels for
+	//each sample--which is not very much.  Mipmapping can give better results.  Find a texturing tutorial that discusses these issues
+	//further.  Conversely, when the texture is magnified (i.e., the texture's texels are *larger* than the screen pixels you're seeing
+	//them on), linearly filter them.  Qualitatively, this causes "blown up" (overmagnified) textures to look blurry instead of blocky.
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	return texture;
+}
+
 extern "C" {
 	void load_2nd_texture_from_memory(unsigned char* bytes, int size)
 	{
@@ -649,8 +699,18 @@ extern "C" {
 		{
 			++pos;
 			char* charbytes = (char*)(bytes);
+			std::string str = charbytes;
+
+			std::string short_str = str.substr(0, 50);
 			std::string decoded = base64_decode(charbytes + pos);
-			gUserData.textureId2 = init_texture_from_memory((unsigned char*)decoded.data(), decoded.size());
+			if (short_str.find("image/jpeg") != std::string::npos) // is jpeg
+			{
+				gUserData.textureId2 = init_jpeg_texture_from_memory((unsigned char*)decoded.data(), decoded.size());
+			}
+			else
+			{
+				gUserData.textureId2 = init_texture_from_memory((unsigned char*)decoded.data(), decoded.size());
+			}
 		}
 	}
 }
@@ -691,7 +751,7 @@ GLuint init_texture_from_memory_wrapper(const char* file)
 	else
 		return 0;
 
-	return init_texture_from_memory(vec.data(), size);
+	return init_jpeg_texture_from_memory(vec.data(), size);
 }
 
 
